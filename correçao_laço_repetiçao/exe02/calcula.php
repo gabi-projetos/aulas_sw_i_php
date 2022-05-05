46f5a13dca39d2157f68c1eff1212b2b246bdbc9
GABRIELA DA SILVA SILVERIO <82417251+gabi-projetos@users.noreply.github.com>
2022-05-05 09:09:40 -0300

    Add files via upload

---
+++ correçao_laço_repetiçao/exe02/calcula.php
@@ -0,0 +1,25 @@
+<?php
+    $numero = $_POST['num'];
+    //echo $numero;
+    $cubo = pow($numero,3);
+    //echo $cubo;
+
+    //Exemplo para indentificar número ímpares
+    //$abacaxi = 10;
+    //$resto = ($abacaxi % 2);
+    //echo $resto;
+
+    for($cont=$numero;$cont<=$cubo;$cont++){
+        if($cont % 2 == 1){
+            echo $cont. "<br>";
+
+        }
+    }
+
+    
+
+
+
+
+
+?>
---
+++ correçao_laço_repetiçao/exe04/calcula.php
@@ -0,0 +1,20 @@
+<?php
+    $numero = $_POST['num'];
+    //echo $numero;
+
+    $resultado = 1;
+    for($cont=$numero;$cont<=1;$cont--){
+            $resultado *= $cont;
+            echo "$cont <br>";
+            echo "$resultado";
+
+
+    }
+    
+    
+
+
+
+
+
+?>
